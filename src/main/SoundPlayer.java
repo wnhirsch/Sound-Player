5ec0f25e29a3358129238de53c60ba9aefcb925c
Iron P Silva <ironprando@gmail.com>
2018-11-20 18:09:22 -0200

    updating branch

--- src/main/SoundPlayer.java
+++ src/main/SoundPlayer.java
@@ -9,7 +9,7 @@ public class SoundPlayer {
 
 	public static void main(String[] argv) {
 		
-		String theString = "0 65463547 0286357 41564fdd12sd v2 3sad4cvs2da152f482 445r284e32sd5f432sd857f4sd325t7445wearusdifkgakdcjrhlfylnudcg ilukhsdafçfidoçwij dçoILKQJÇ \\,.X/.\\ZX.,ÇX, |ZX, XÇ";
+		String theString = "a0 6a5463547 02df86357 v2 3sadsdf4cvs2gsda152f482 445r284edg32sd5f432sd857f4sd325t7445wearusdifkgakdcjrhlfylnudcg ilukhsdafçfidoçwij dçoILKQJÇ \\,.X/.\\ZX.,ÇX, |ZX, XÇ";
 		OuterControl ctrl = new OuterControl(theString, OuterControl.ParserType.V1);
 		
 		ctrl.saveMidiFile();
